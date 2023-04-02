c8570833b2ac228ea5cd9caf64583f13e6368be7
meskud <118122001+meskud@users.noreply.github.com>
2023-04-02 19:18:40 +0200

    Working placeholder for 'smjerovi'

--- myworld/my_tennis_club/members/urls.py
+++ myworld/my_tennis_club/members/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('', views.home, name='home'),
+    path('smjerovi/', views.smjerovi, name='smjerovi'),
 ]
--- myworld/my_tennis_club/members/views.py
+++ myworld/my_tennis_club/members/views.py
@@ -4,3 +4,11 @@ from django.template import loader
 def members(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def home(request):
+  template = loader.get_template('home.html')
+  return HttpResponse(template.render())
+
+def smjerovi(request):
+  template = loader.get_template('smjerovi.html')
+  return HttpResponse(template.render())
