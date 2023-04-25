b1025b157166f291934a8c2c1807a94176b0d92e
meskud <118122001+meskud@users.noreply.github.com>
2023-04-25 11:33:37 +0200

    Added many urls and views so buttons actually do something

--- myworld/my_tennis_club/members/urls.py
+++ myworld/my_tennis_club/members/urls.py
@@ -7,6 +7,12 @@ smjerovi = ["mehatronika", "mašinstvo", "tekstilstvo"]
 
 urlpatterns = [
     path('members/', views.members, name='members'),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  
+    path('members/', views.members, name='members'),
+    path('Vijesti/', views.vijesti, name='vijesti'),
+    path('Upis/', views.upis, name='upis'),
+    path('Prostor/', views.prostor, name='prostor'),
     path('smjerovi/<slug:slug>', views.smjerovi, name='smjerovi'),
+    path('Događaji/', views.događaji, name='događaji'),
+    path('Kontakt/', views.kontakt, name='kontakt'),
 ]
--- myworld/my_tennis_club/members/views.py
+++ myworld/my_tennis_club/members/views.py
@@ -8,7 +8,19 @@ def members(request):
   return HttpResponse(template.render())
 
 def home(request):
-  template = loader.get_template('home.html')
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
+
+def kontakt(request):
+  template = loader.get_template('contact.html')
+  return HttpResponse(template.render())
+
+def upis(request):
+  template = loader.get_template('upis.html')
+  return HttpResponse(template.render())
+
+def prostor(request):
+  template = loader.get_template('prostor.html')
   return HttpResponse(template.render())
 
 def smjerovi(request, slug):
@@ -16,3 +28,16 @@ def smjerovi(request, slug):
   template = loader.get_template('smjerovi.html',)
   context = {'articles': articles}
   return HttpResponse(template.render(context, request))
+
+def vijesti(request, slug):
+  articles = Article.objects.all()
+  template = loader.get_template('news2.html',)
+  context = {'articles': articles}
+  return HttpResponse(template.render(context, request))
+  #"Articles" context part was unchanged after copy paste of earlier view
+
+def događaji(request, slug):
+  articles = Article.objects.all()
+  template = loader.get_template('news.html',)
+  context = {'articles': articles}
+  #The html for events tab is missing so news html was implemented instead
