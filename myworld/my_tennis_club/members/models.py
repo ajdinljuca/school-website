c55cdb25963a5b8dc3fbf989e787e994cd2cce70
meskud <118122001+meskud@users.noreply.github.com>
2023-04-30 16:08:57 +0200

    Added many changes to models, some of the django syntax to html files, tried some dynamic url generation, but no success, will try to solve it differently. New code was added in separate blocks so I can track what's broken more easily, so far so good.

--- myworld/my_tennis_club/members/models.py
+++ myworld/my_tennis_club/members/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# New import (not tested)
+from django.utils.text import slugify
 
 
 class Article(models.Model):
@@ -10,3 +12,33 @@ class Article(models.Model):
 
   def __str__(self):
     return f"{self.title}"
+  
+  
+
+# This might produce some unwanted bugs:
+# The following code defines models for professions tab so the urls can 
+# generate properly
+
+
+class Struka(models.Model):
+  name = models.CharField(max_length=50)
+  slug = models.SlugField(unique=True, editable=False)
+
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.name)
+    super().save(*args, **kwargs)
+
+  def __str__(self):
+    return self.name
+
+class Smijer(models.Model):
+  name = models.CharField(max_length=50)
+  slug = models.SlugField(unique=True, editable=False)
+  struka = models.ForeignKey(Struka, on_delete=models.CASCADE)
+
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.name)
+    super().save(*args, **kwargs)
+
+    def __str__(self):
+      return self.name
